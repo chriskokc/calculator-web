bd894ea9e33dd22eae167da9b47e6c073b85b397
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-01 09:53:32 +0100

    renamed some class for the functional buttons and added pseudocode for the script.

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// handle user input
+// handle user mouse clicks on keypads
+// handle user key press
+
+// perform calculation
+// store the following variables:
+// first number
+// operator
+// second number
+// functions required:
+// addition
+// subtraction
+// multiplication
+// division
+
+// return the output and display it as an output
+// handle user mouse clicks on the equal button
+
+// reset calculator
+// reset the input as 0
+// reset the output as output
+// handle user mouse clicks on the clear button
